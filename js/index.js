a2f6bccce222bbdea05bb89a8f0337292216a047
wxx <1419350157@qq.com>
2016-12-01 16:32:01 +0800

    最后

--- js/index.js
+++ js/index.js
@@ -18,20 +18,62 @@ $(function(){
     //banner轮播
     var imag_box=$('.images');
     var banner=$('.banner-left');
-    var x=$('.image-list').width();
     var b=$('.image-list');
     var t=setInterval(move,2000);
     var i=0;
     function move(){
+        var x=$('.image-list').width();
     	i++;
     	if (i==b.length) {
     		i=0;
     	}
     	imag_box.animate({left:x*-i},1000)
+        b_btn.removeClass('hot').eq(i).addClass('hot');
     }
-    banner.hover(function(){
-    	clearInterval(t)
-    },function(){
-    	t=setInterval(move,2000)
+    // banner.hover(function(){
+    // 	clearInterval(t)
+    // },function(){
+    // 	t=setInterval(move,2000)
+    // })
+    var b_btn=$('.banner-btn');
+    b_btn.eq(0).addClass('hot');
+    b_btn.click(function(){
+        clearInterval(t);
+        i=b_btn.index(this);
+        var x=$('.image-list').width();
+        b_btn.removeClass('hot').eq(i).addClass('hot');
+        imag_box.animate({left:x*-i},1000,function(){
+            var t=setInterval(move,2000);
+        });
+    })
+    var b_left=$('.btn-left');
+    var b_right=$('.btn-right');
+    b_right.click(function(){
+        var x=$('.image-list').width();
+       clearInterval(t)
+            i++
+            if (i==b.length) {
+                i=0;
+            };
+            imag_box.animate({left:x*-i},1000)
+            b_btn.removeClass('hot').eq(i).addClass('hot');
+            t=setInterval(move,1500)
+    })
+    b_left.click(function(){
+        var x=$('.image-list').width();
+         clearInterval(t)
+            i--
+            if (i==-1) {
+                i=b.length-1;
+            };
+            imag_box.animate({left:x*-i},1000)
+            b_btn.removeClass('hot').eq(i).addClass('hot');
+            t=setInterval(move,1500)
+    })
+    //返回顶部
+    var top=$('.back');
+    top.click(function(){
+        $('html,body').animate({scrollTop:0},1000)
+        return false;
     })
 })
